2682530896fc637a5f7d26d8635e91f92d6e10d6
Max Shvets <maxim.shvets@gmail.com>
2019-06-08 13:22:18 +0300

    Removed redundant code from basic line fitting example

--- examples/basic_line_fitting_1.py
+++ examples/basic_line_fitting_1.py
@@ -2,13 +2,6 @@ from ransac import ransac_fit
 from line_fitting import LineModel, plot_results
 
 
-if __name__ == "__main__":
-    data = {(0, 0), (1, 1), (2, 2), (3.5, 2), (3, 3), (4, 4), (10, 2)}
-    fit = ransac_fit(data, LineModel, 2, 21, 0.01, 4)
-
-    print("Params: ")
-    print(fit.get_params())
-    print("Points: ")
-    print(fit.get_points())
-
-    plot_results(data, fit.get_points(), fit.get_params())
+data = {(0, 0), (1, 1), (2, 2), (3.5, 2), (3, 3), (4, 4), (10, 2)}
+fit = ransac_fit(data, LineModel, 2, 21, 0.01, 4)
+plot_results(data, fit.get_points(), fit.get_params())
