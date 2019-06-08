d594e1b2c464ca6fbb6123ccf8e6bb262674c3fd
Max Shvets <maxim.shvets@gmail.com>
2019-06-08 12:44:49 +0300

    Moved LineModel to the separate file

--- line_model.py
+++ line_model.py
@@ -1,16 +1,3 @@
-from ransac import ransac_fit
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def abline(intercept, slope):
-    """Plot a line from slope and intercept"""
-    axes = plt.gca()
-    x_vals = np.array(axes.get_xlim())
-    y_vals = intercept + slope * x_vals
-    plt.plot(x_vals, y_vals, '--')
-
-
 class LineModel:
     def __init__(self, points):
         self._points = points
@@ -37,19 +24,3 @@ class LineModel:
 
     def get_points(self):
         return self._points
-
-
-if __name__ == "__main__":
-    data = {(0, 0), (1, 1), (2, 2), (3.5, 2), (3, 3), (4, 4), (10, 2)}
-    fit = ransac_fit(data, LineModel, 2, 21, 0.01, 4)
-
-    print("Params: ")
-    print(fit.get_params())
-    print("Points: ")
-    print(fit.get_points())
-
-    for x, y in data:
-        plt.scatter(x, y, None, 'r' if (x, y) in fit.get_points() else 'b')
-
-    abline(*fit.get_params())
-    plt.show()
