3ea1473a653d23129c20b04d7ab8c40e9d9c96d7
Max Shvets <maxim.shvets@gmail.com>
2019-06-16 13:55:01 +0300

    Updated code formatting in the random_line_fitting.py

--- examples/random_line_fitting.py
+++ examples/random_line_fitting.py
@@ -16,14 +16,20 @@ def conduct_random_experiment():
     step = (x_bounds[1] - x_bounds[0]) / n
     min_model_size = 2
     inlier_probability = 1 - outlier_probability
-    max_trials_amount = compute_trials_amount(inlier_probability, min_model_size)
+    max_trials_amount = compute_trials_amount(
+        inlier_probability, min_model_size
+    )
     min_accepted_size = n * inlier_probability - 20
 
     def random_error():
         is_error = random.random() < outlier_probability
 
         if is_error:
-            return tolerance * random.uniform(2, 20) * random.choice([-1, 1])
+            return (
+                tolerance
+                * random.uniform(2, 20)
+                * random.choice([-1, 1])
+            )
         else:
             return tolerance * random.uniform(-1, 1)
 
@@ -32,13 +38,17 @@ def conduct_random_experiment():
         points.add((x, y))
         x += step * random.random()
 
-    fit = ransac_fit(points, LineModel, min_model_size, max_trials_amount, tolerance, min_accepted_size)
+    fit = ransac_fit(
+        points, LineModel, min_model_size,
+        max_trials_amount, tolerance, min_accepted_size
+    )
 
     return {
         "fit": fit,
         "real_params": (intercept, slope),
         "points": points,
-        "fitting_model_found": min_accepted_size <= len(fit.get_points())
+        "fitting_model_found":
+            min_accepted_size <= len(fit.get_points())
     }
 
 
@@ -46,7 +56,10 @@ def calculate_fit_wellness(result):
     real_intercept, real_slope = result["real_params"]
     fitted_intercept, fitted_slope = result["fit"].get_params()
 
-    return (fitted_intercept - real_intercept)**2 + (fitted_slope - fitted_slope)**2
+    return (
+        (fitted_intercept - real_intercept)**2
+        + (fitted_slope - fitted_slope)**2
+    )
 
 
 def conduct_random_experiment_series():
@@ -59,13 +72,19 @@ def conduct_random_experiment_series():
             results.append(result)
 
     print("Successful experiments: ", len(results))
-    worst_fit_result_index = np.argmax(map(calculate_fit_wellness, results))
+    worst_fit_result_index = np.argmax(
+        map(calculate_fit_wellness, results)
+    )
     worst_fit_result = results[worst_fit_result_index]
     worst_fit = worst_fit_result["fit"]
 
     print("Worst fit real params: ", worst_fit_result["real_params"])
     print("Worst fit fitted params: ", worst_fit.get_params())
-    plot_results(worst_fit_result["points"], worst_fit.get_points(), worst_fit.get_params())
+    plot_results(
+        worst_fit_result["points"],
+        worst_fit.get_points(),
+        worst_fit.get_params()
+    )
 
 
 conduct_random_experiment_series()
